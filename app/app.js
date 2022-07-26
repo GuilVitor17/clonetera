9e5ec8a32cf0cf2f26c8404b289814d2c7e90576
@Guilvitor17 <109220774+GuilVitor17@users.noreply.github.com>
2022-07-26 14:18:38 -0300

    tera
    tera

---
+++ app/app.js
@@ -0,0 +1,12 @@
+function abri(){
+  var ul = document.getElementById('ul')
+  ul.style.display='block'
+}
+console.log(abri)
+
+
+function sair(){
+    var ul = document.getElementById('ul')
+
+    ul.style.display='none'
+}
